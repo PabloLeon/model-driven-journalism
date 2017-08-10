63bf710c60fab03de95ced4eecf3036997f74601
PabloLeon <pablo.leon@gmx.de>
2017-08-10 12:28:03 +0100

    restructuring and packages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from "react";
-import "./App.css";
-import Article from "./Article";
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Article from './components/Article';
 
 class App extends Component {
   render() {
-    return (
-      <Article />
-    );
+    return <Article />;
   }
 }
-
 export default App;
