daec833d6d26fe42ce001164bc7913f2809d6b91
PabloLeon <pablo.leon@gmx.de>
2017-08-22 10:57:47 +0100

    range slider look done

--- src/components/Range.js
+++ src/components/Range.js
@@ -2,37 +2,21 @@ import React from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css'; // I would like to get rid of all these dependencies in the future
 
-const style = { width: 400, margin: 50 };
-const marks = {
-  '-10': '-10°C',
-  0: <strong>0°C</strong>,
-  26: '26°C',
-  37: '37°C',
-  50: '50°C',
-  100: {
-    style: {
-      color: 'red',
-    },
-    label: <strong>100°C</strong>,
-  },
+const Range = (props) => {
+  const { value, step, min, max, marks } = props;
+  return (
+    <div>
+      <Slider
+        dots
+        min={min}
+        max={max}
+        step={step}
+        onChange={() => console.log('lala')}
+        defaultValue={value}
+        marks={marks}
+      />
+    </div>
+  );
 };
-
-function log(value) {
-  console.log(value); //eslint-disable-line
-}
-
-const Range = props =>
-  // const { value, step, defaultValue, min, max } = props;
-  (<div>
-    <div style={style}>
-      <p>Slider with marks and steps</p>
-      <Slider dots min={-10} marks={marks} step={10} onChange={log} defaultValue={20} />
-    </div>
-
-    <div style={style}>
-      <p>Slider with marks, `included=false`</p>
-      <Slider min={-10} marks={marks} included={false} defaultValue={20} />
-    </div>
-  </div>);
 
 export default Range;
--- src/components/RangeBlock.js
+++ src/components/RangeBlock.js
@@ -6,16 +6,56 @@ const styles = {};
 
 class RangeBlock extends Component {
   render() {
+    const { header, info, range } = this.props;
     return (
       <div>
-        <h1>RangeBlock</h1>
-        <Range min={0} max={100} />
+        <h1>
+          {header}
+        </h1>
+        <p>
+          {' '}{info}{' '}
+        </p>
+        <Range min={range.min} max={range.max} step={range.step} marks={range.marks} />
       </div>
     );
   }
 }
-RangeBlock.propTypes = {};
+RangeBlock.propTypes = {
+  selectedValue: PropTypes.number,
+  hoverMark: PropTypes.number,
+  header: PropTypes.string.isRequired,
+  info: PropTypes.string.isRequired,
+  range: PropTypes.shape({
+    min: PropTypes.number.isRequired,
+    max: PropTypes.number.isRequired,
+    steps: PropTypes.number,
+    marks: PropTypes.object,
+  }).isRequired,
+  onOK: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
 
-RangeBlock.defaultProps = {};
+RangeBlock.defaultProps = {
+  selectedValue: undefined,
+  hoverMark: undefined,
+  header: 'RangeBlock default header',
+  info: 'RangeBlock default info',
+  range: {
+    min: 0,
+    max: 100,
+    step: 10,
+    marks: {
+      0: '0%',
+      50: '50%',
+      85: {
+        style: {
+          color: 'orange',
+        },
+        label: 'The NHS target',
+      },
+      100: '100%',
+    },
+  },
+};
 
 export default RangeBlock;
